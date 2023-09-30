6de902abc1366312c1039feaa380b0f22287c245
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-09-30 17:53:12 +0900

    first commit

---
+++ part1/003.js
@@ -0,0 +1,4 @@
+console.log("Visual Stodio Code로 코드 실행보기");
+var a = 5;
+var a = 2;
+console.log(a + b);
---
+++ part1/004.js
@@ -0,0 +1,4 @@
+console.log("Visual Stodio Code로 코드로 실행보기");
+var a = 5;
+var b = 2; 
+console.log(a + b);
