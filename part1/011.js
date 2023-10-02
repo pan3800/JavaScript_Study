76218a10aba8b62e6068dcc51fd3f7abb6f31ca0
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-10-02 15:51:43 +0900

    if 조건문 커밋

---
+++ part1/011.js
@@ -0,0 +1,10 @@
+var number = 2;
+if (number == 1) {
+    console.log('number는 1 입니다.')
+} else if (number == 2) {
+    console.log('number 2 입니다.')
+} else if (number == 3) {
+    console.log('number 3 입니다.')
+} else {
+    console.log('1, 2, 3, 중 해당 사항에 없습니다.');
+}
