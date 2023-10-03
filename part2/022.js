b904c0abaa94bf4c71b47e411a27cad86b35bb28
Chae Seong Lim <58936137+pan2468@users.noreply.github.com>
2023-10-03 22:49:48 +0900

    자바스크립트 연산자 사용예시

---
+++ part2/022.js
@@ -0,0 +1,9 @@
+console.log(5 == 5);
+console.log("5" == 5);
+console.log(5 == '5');
+console.log(5 != "1");
+console.log(5 != "5");
+console.log(5 === 5);
+console.log(5 === "5");
+console.log(5 != 10);
+console.log(5 != "5");
