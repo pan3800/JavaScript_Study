63baf4f2ac4d89c01a41580eb049f1ed3334dc59
Chae Seong Lim <pan1129@naver.com>
2023-10-15 18:56:46 +0900

    const 변수 사용법

---
+++ part2/042-1.js
@@ -0,0 +1,7 @@
+const CONST_USER = {name: 'jay', age: 30};
+console.log(CONST_USER.name, CONST_USER.age);
+CONST_USER.name = 'jay2';
+CONST_USER.age = 31;
+console.log(CONST_USER.name, CONST_USER.age);
+CONST_USER = {name: 'bbo'}
+console.log(CONST_USER.name);
